eef7b7d90a63de7fd73b7b9de018761bda9f6ccc
Shintaro Katayama <shintaro.katayama@gmail.com>
2016-08-21 01:52:47 +0200

    add "--buffer-size" option for fq1l nr

--- lib/bio/gadget/fq1l/nr.rb
+++ lib/bio/gadget/fq1l/nr.rb
@@ -4,13 +4,22 @@ module Bio
       
       desc 'nr', 'Extract non-redundant best-quality sequences, esp. for UMI'
       
+      method_option :buffer_size,
+                    banner: 'SIZE',
+                    aliases: '-S',
+                    desc: 'Use SIZE for main memory buffer'
+      
       def nr
         pseq = ''
         prefix = options.prefix_coreutils
-        open("| #{prefix}sort --parallel=`#{prefix}nproc` -r -k2,4").each do |line|
+        command = <<CMD
+| #{prefix}sort --parallel=`#{prefix}nproc` -r -k2,4 \
+    #{options.key?('buffer_size') ? '-S '+options.buffer_size : ''}
+CMD
+        open(command).each do |line|
           acc, seq, tmp, qual = line.split /\t/
           if pseq != seq
-            puts line.strip
+            puts line.rstrip
             pseq = seq
           end
         end
