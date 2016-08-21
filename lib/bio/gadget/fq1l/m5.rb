864db9926bf0832291a53d8f45a4e985b0c66805
Shintaro Katayama <shintaro.katayama@gmail.com>
2016-08-21 22:08:19 +0200

    add "--invert-match" option for "fq1l m5"

--- lib/bio/gadget/fq1l/m5.rb
+++ lib/bio/gadget/fq1l/m5.rb
@@ -4,9 +4,14 @@ module Bio
       
       desc 'm5 PATTERN',
            'Select sequences that match the 5\'-end with a given PATTERN'
+
+      method_option :invert_match,
+                    type: :boolean,
+                    aliases: '-v',
+                    desc: 'Invert the sense of matching, to select non-matching lines'
       
       def m5(pattern)
-        exec "#{options.prefix_coreutils}grep -P -e '^[^\\t]+\\t#{pattern}'"
+        exec "#{options.prefix_coreutils}grep #{options.invert_match ? '-v' : ''} -P -e '^[^\\t]+\\t#{pattern}'"
       end
       
     end
