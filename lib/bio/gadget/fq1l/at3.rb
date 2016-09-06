0630953dfe555189223e4271a70f2da23d45d65a
Shintaro Katayama <shintaro.katayama@gmail.com>
2016-09-07 01:23:47 +0200

    optimize fq1l:at3

--- lib/bio/gadget/fq1l/at3.rb
+++ lib/bio/gadget/fq1l/at3.rb
@@ -5,68 +5,44 @@ module Bio
       desc 'at3 ADAPTOR', 'Adaptor trimming, 3\'-end'
 
       def at3(adaptor)
-        seqs = Hash.new
+        fragments = Hash.new
         max = adaptor.length-1
         tmp = Hash.new
         for i in 0..max do
           for j in i..max do
-            seq = adaptor[i..j]
-            unless tmp.key?(seq)
-              len = seq.length
-              seqs[len] = Array.new unless seqs.key?(len)
-              seqs[len] << seq
-              tmp[seq] = true
+            fragment = adaptor[i..j]
+            unless tmp.key?(fragment)
+              l = fragment.length
+              fragments[l] = Array.new unless fragments.key?(l)
+              fragments[l] << fragment
+              tmp[fragment] = true
             end
           end
         end
         #
-        keys = Array.new
-        seqs.keys.sort.reverse.each do |len|
-          keys << len
-          break if 4**len == seqs[len].length
-        end
-        tmps = Array.new(keys.length) do
-          Bio::Gadgets.getTmpname('fq1l.at3', 'fq1l', false)
-        end
-        pipes = Array.new(keys.length-1) { IO.pipe }
-        pipes = [nil, pipes.flatten.reverse, nil].flatten
-        keys.each_index do |i|
-          len = keys[i]
-          tmp = tmps[i]
-          r, w = pipes.shift 2
-          Kernel.fork do
-            STDIN.reopen r if r
-            STDOUT.reopen w if w
-            t3(len, seqs[len], tmp)
-          end
-          r.close if r
-          w.close if w
-        end
-        Process.waitall
-        #
-        exec "cat #{tmps.join(' ')}"
-      end
-      
-      no_commands do
-        
-        def t3(len, seqs, trimmed)
-          fifo = Bio::Gadgets.mkfifo('fq1l.t3', 'fq1l')
-          prefix = options.prefix_coreutils
-          if 4**len == seqs.length
-            pCmds = "#{prefix}cat > #{fifo}"
-            cCmds = "#{prefix}cat #{fifo}"
+        prefix = options.prefix_coreutils
+        main = ''
+        tmpfiles = Array.new
+        fragments.keys.sort.reverse.each do |length|
+          fifo = Bio::Gadgets.mkfifo('fq1l.at3', 'fq1l')
+          tmpfiles << tmpfile = Bio::Gadgets.getTmpname('fq1l.at3', 'fq1l', false)
+          if 4**length == fragments[length].size
+            main += "#{prefix}cat > #{fifo}"
+            Kernel.fork do
+              BioGadget.trim3(length, "#{prefix}cat #{fifo}", tmpfile)
+            end
           else
-            patterns = (seqs.map { |seq| "-e '#{seq}\t+'" }).join ' '
-            pCmds = "#{prefix}tee #{fifo} | #{prefix}grep -v #{patterns}"
-            cCmds = "#{prefix}grep #{patterns} #{fifo}"
+            patterns = (fragments[length].map { |fragment| "-e '#{fragment}\t+'" }).join ' '
+            main += "#{prefix}tee #{fifo} | #{prefix}grep -v #{patterns} | "
+            Kernel.fork do
+              BioGadget.trim3(length, "#{prefix}grep #{patterns} #{fifo}", tmpfile)
+            end
           end
-          pid = Kernel.fork do
-            exec pCmds
-          end
-          BioGadget.trim3(len, cCmds, trimmed)
-          Process.wait(pid)
         end
-        
+        tmpfiles.map { |tmpfile| at_exit { File.unlink(tmpfile) if FileTest.exist?(tmpfile) } }
+        system main
+        system "cat #{tmpfiles.join(' ')}"
+        Process.waitall
       end
       
     end
