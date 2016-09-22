a4b19c8322c476937905f0b98b5010f28eaafa20
Shintaro Katayama <shintaro.katayama@gmail.com>
2016-09-23 00:53:36 +0200

    add command strt:count

--- lib/bio/gadget/strt.rb
+++ lib/bio/gadget/strt.rb
@@ -1,4 +1,5 @@
 require 'bio/gadget/strt/bam2bed5p.rb'
+require 'bio/gadget/strt/count.rb'
 require 'bio/gadget/strt/prepBed.rb'
 require 'bio/gadget/strt/prepGtf.rb'
 require 'bio/gadget/strt/prepSeq.rb'
---
+++ lib/bio/gadget/strt/count.rb
@@ -0,0 +1,63 @@
+require 'parallel'
+
+module Bio
+  module Gadget
+    class STRT < Thor
+
+      desc 'count SMP BASE BED [BED ...]',
+           "Count 5'-ends at BASE in each region defined by BEDs"
+      
+      method_option :parallel,
+                    aliases: '-p',
+                    banner: 'N',
+                    default: system('which gnproc >/dev/null 2>&1') ? `gnproc`.to_i : (system('which nproc >/dev/null 2>&1') ? `nproc`.to_i : 2),
+                    desc: 'Change the number of sorts run concurrently to N',
+                    type: :numeric
+      
+      method_option :prefix_coreutils,
+                    banner: 'PREFIX',
+                    default: system('which gnproc >/dev/null 2>&1') ? 'g' : '',
+                    desc: 'A prefix character for GNU coreutils',
+                    type: :string
+
+      def count(smp, base, bed0, *beds)
+
+        cPrefix = options.prefix_coreutils
+        
+        smps = Hash.new
+        fp = open(smp)
+        header = fp.gets.rstrip.split(',')
+        idxName = header.index('NAME')
+        idxBeds = header.index('5pBEDs')
+        fp.each do |line|
+          cols = line.rstrip.split(',')
+          smps[cols[idxName]] = cols[idxBeds].split(';')
+        end
+        fp.close
+
+        tmpfile = Bio::Gadgets.getTmpname('strt.count', 'bed')
+        system "#{cPrefix}sort -k 1,1 -k 2,2n -k 3,3n -k 4,4 #{bed0} #{beds.join(' ')} > #{tmpfile}"
+
+        counts = Hash.new
+        mutex = Mutex.new
+        Parallel.map(smps.keys, in_threads: options.parallel) do |name|
+          bed5ps = smps[name].map { |bed| "#{base}#{bed}.5p.bed.gz" }
+          open("| bedtools intersect -nonamecheck -wa -wb -s -sorted -a #{tmpfile} -b #{bed5ps.join(' ')} | #{cPrefix}cut -f 4,10 | #{cPrefix}sort -u | #{cPrefix}cut -f 1 | #{cPrefix}uniq -c").each do |line|
+            cnt, id = line.strip.split(' ')
+            mutex.synchronize do
+              counts[id] = Hash.new unless counts.key?(id)
+              counts[id][name] = cnt.to_i
+            end
+          end
+        end
+
+        names = smps.keys.sort
+        puts (['ID'] + names.map { |name| "R|#{name}" }).join(',')
+        counts.each do |id, name2count|
+          puts ([id] + names.map { |name| name2count.key?(name) ? name2count[name] : 0 }).join(',')
+        end
+      end
+      
+    end
+  end
+end
